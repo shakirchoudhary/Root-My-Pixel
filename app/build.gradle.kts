455d3555905b09e4a6e0bdea1222e2e3a520c6c5
Shakir Choudhary <shakir.choudhary@fatakpay.com>
2026-08-18 11:50:34 +0530

    running changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 android {
     namespace = "com.alex193a.rootmypixel"
     compileSdk = 37
+    ndkVersion = "29.0.13113456"
 
     defaultConfig {
         applicationId = "com.alex193a.rootmypixel"
